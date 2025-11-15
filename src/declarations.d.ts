8ab93f89b73135d6dcb87a07775d61cfbd6b5183
sanjeevkumar2662mint <sanjeevkumar3662work@gmail.com>
2025-11-15 13:44:23 +0530

    chore: update ts declarations

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,15 +1,27 @@
 // src/declarations.d.ts
-declare module '*.webp' {
+declare module "*.webp" {
   const value: string;
   export default value;
 }
 
-declare module '*.png' {
+declare module "*.png" {
   const value: string;
   export default value;
 }
 
-declare module '*.jpg' {
+declare module "*.jpg" {
   const value: string;
   export default value;
+}
+
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SERVER_URI: string;
+  // Add more if needed
+  // readonly VITE_WHATEVER: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
